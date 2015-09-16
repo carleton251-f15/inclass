e1c448948664120a9d22951584a6b421a3d69819
Dave Musicant (Carleton VM) <dmusican@carleton.edu>
2015-09-16 12:20:19 -0500

    more

--- day2.rkt
+++ day2.rkt
@@ -4,4 +4,41 @@
 
 (define add-two-nums
   (lambda (x y)
-    (+ x y)
+    (+ x y)))
+
+(define list-length
+  (lambda (lst)
+    (if (null? lst)
+        0
+        (+ 1 (list-length (cdr lst))))))
+
+;; Test to see if a list is a list of numbers
+(define list-of-numbers?
+  (lambda (lst)
+    (if (null? lst)
+        #t
+        (and (number? (car lst))
+             (list-of-numbers? (cdr lst))))))
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
