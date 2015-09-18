ce10b05bef86eeff2d442ea42ca285f058d533e4
Dave Musicant (Carleton VM) <dmusican@carleton.edu>
2015-09-18 12:21:29 -0500

    day 3

---
+++ day3.rkt
@@ -0,0 +1,21 @@
+(define my-remove-first
+  (lambda (item lst)
+    (cond [(null? lst) '()]
+          [(equal? item (car lst))  (cdr lst)]
+          [else (cons (car lst) (my-remove-first item (cdr lst)))])))
+
+(my-remove-first 2 '(1 3 9 2 8 2 6 12))
+
+(define my-remove-all
+  (lambda (item lst)
+    (cond [(null? lst) '()]
+          [(equal? item (car lst))  (my-remove-all item (cdr lst))]
+          [else (cons (car lst) (my-remove-all item (cdr lst)))])))
+
+(my-remove-all 2 '(1 3 9 2 8 2 6 12))
+
+
+
+(define subst
+  (lambda (old new lst)
+    (cond [
