82746c3df7ef64da3271ecdfb8b24337f6b63c46
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2015-10-09 12:39:49 -0500

    added literals

---
+++ literals.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    //char *orig = "chalk";
+    char word[6];
+    word[5] = "chalk";
+    /*char word[6] = "chalk";
+    //strcpy(word,orig);
+    strcpy(word,"chalk");
+    word[1] = 'b';
+    printf("%s\n",word);
+    
+    char *x = "hello";
+    x[1] = 'q';
+    printf("%s\n",x);
+    printf("hello");*/
+}
